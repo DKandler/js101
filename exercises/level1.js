55f221d2c768021e5a211661eff122cb6540b827
David Kandler <davidkandler@gmail.com>
2019-05-04 12:30:45 -0400

    answered all 6

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,47 @@
 function lvl1exercise1 () {
   // Declare a variable without instantiating it and return it!
-
+var var1;
+return var1;
 }
 
 function lvl1exercise2 () {
   // Declare and instantiate a number and return it
-
+var var2 = 2;
+return var2;
 }
 
 function lvl1exercise3 () {
   // Declare and instantiate a floating point number that is not a whole number and return it
-
+var var3 = 2.1;
+return var3;
 }
 
 function lvl1exercise4 () {
   // Declare and instantiate a string "Hello World!" and return it
-
+var var4 = 'Hello World!';
+return var4;
 }
 
 function lvl1exercise5 () {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+var var5 =[
+  "Hello World!",
+  4
+]
+return var5;
 }
 
 function lvl1exercise6 () {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+var var6 = {
+  key1: "Hello World!",
+  key2: 4,
+};
+return var6;
 }
 
 function lvl1exercise7 () {
   // Declare and instantiate a boolean value 'false' and return it
-
+var var7 = false;
+return var7;
 }
--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,27 @@
 function lvl3exercise1 () {
   // Create a "hello" and a "world", return the concatenation of the two
-
+return("hello" + "world");
 }
 
 function lvl3exercise2 () {
   // Create a "hello" and a 12, return the concatenation of the two
-
+return('hello' + 12);
 }
 
 function lvl3exercise3 () {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+var twelve = 12;
+return(twelve + '');
 }
 
 function lvl3exercise4 () {
   // Create a "hello world!" string. Return the length of the string
-
+var helloWorld = 'hello world!'
+return (helloWorld.length);
 }
 
 function lvl3exercise5 () {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  var helloWorld2 = 'hello world!'
+  return(helloWorld2.indexOf('world'));
 }
